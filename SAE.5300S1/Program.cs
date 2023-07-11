bb50d423c2b3d661a6caaa00014cd45caab873af
Roman Aebi <unity2.sae@gmail.com>
2023-07-11 17:25:22 +0200

    first commit

---
+++ SAE.5300S1/Program.cs
@@ -0,0 +1,7 @@
+﻿// If you're using an earlier version of .NET which doesn't have file-scoped namespaces (.NET 5 or earlier), you'll need to encase your class inside the namespace.
+namespace SAE._5300S1;
+
+public class Program
+{
+    public static void Main(string[] args) { }
+}
