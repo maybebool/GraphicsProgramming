6d744f174d3fb3107baab222c1ec60e7cf342c80
Roman Aebi <unity2.sae@gmail.com>
2023-07-12 19:02:18 +0200

    create simple window

--- SAE.5300S1/Program.cs
+++ SAE.5300S1/Program.cs
@@ -1,9 +1,48 @@
-﻿// If you're using an earlier version of .NET which doesn't have file-scoped namespaces (.NET 5 or earlier), you'll need to encase your class inside the namespace.
+﻿using Silk.NET.OpenGL;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+
 namespace SAE._5300S1;
 
 public class Program
 {
-    public static void Main(string[] args) {
+    
+    private static GL _gl;
+    private static IWindow _window;
 
+    public static void Main(string[] args) {
+        
+        var options = WindowOptions.Default with
+        {
+            Size = new Vector2D<int>(800, 600),
+            Title = "My first Silk.NET application!"
+        };
+        
+        _window = Window.Create(options);
+        _window.Load += OnLoad;
+        _window.Update += OnUpdate;
+        _window.Render += OnRender;
+        _window.Run();
+        
+        
+
+    }
+
+
+    private static void OnLoad() {
+        IInputContext input = _window.CreateInput();
+        for (int i = 0; i < input.Keyboards.Count; i++)
+            input.Keyboards[i].KeyDown += KeyDown;
+    }
+
+    private static void OnUpdate(double dt) { }
+
+    private static void OnRender(double dt) { }
+
+    private static void KeyDown(IKeyboard keyboard, Key key, int keyCode) {
+        if (key == Key.Escape)
+            _window.Close();
     }
 }
