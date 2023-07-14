aed8d3126b6341cc23f8aad21502e728ca6a7851
Roman Aebi <unity2.sae@gmail.com>
2023-07-14 14:07:26 +0200

    buffer scripts

--- SAE.5300S1/Program.cs
+++ SAE.5300S1/Program.cs
@@ -8,235 +8,180 @@ using System.Numerics;
 
 namespace SAE._5300S1;
 
-public class Program
-{
-    // Window + OpenGL
-    private static GL _gl;
-    private static IWindow _window;
+public class Program {
+    private static IWindow window;
+    private static GL Gl;
     private static IKeyboard primaryKeyboard;
-    
-    // Buffers + Program
-    private static uint _vao;
-    private static uint _vbo, _ebo;
-    private static uint _program;
-    
-    
-    // Inputs
-    private static Vector2 LastMousePosition;
-    private static Vector3 CameraPosition = new(0.0f, 0.0f, 3.0f);
-    private static Vector3 CameraFront = new(0.0f, 0.0f, -1.0f);
+
+    private const int Width = 800;
+    private const int Height = 700;
+
+    private static BufferObject<float> Vbo;
+    private static BufferObject<uint> Ebo;
+    private static VertexArrayObject<float, uint> Vao;
+    private static Texture Texture;
+    private static Shader Shader;
+
+    //Setup the camera's location, directions, and movement speed
+    private static Vector3 CameraPosition = new Vector3(0.0f, 0.0f, 3.0f);
+    private static Vector3 CameraFront = new Vector3(0.0f, 0.0f, -1.0f);
     private static Vector3 CameraUp = Vector3.UnitY;
     private static Vector3 CameraDirection = Vector3.Zero;
     private static float CameraYaw = -90f;
     private static float CameraPitch = 0f;
     private static float CameraZoom = 45f;
-    
-    static float[] vertices =
-    {
-        0.5f,  0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
-    };
-    
-    
-    
-    static uint[] indices =
-    {
-        0u, 1u, 3u,
-        1u, 2u, 3u
+
+    //Used to track change in mouse movement to allow for moving of the Camera
+    private static Vector2 LastMousePosition;
+
+    private static readonly float[] Vertices = {
+        //X    Y      Z     U   V
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+        -0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
+
+        -0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, 1.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f, 0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, 0.0f, 0.0f
     };
 
-    public static void Main(string[] args) {
-        
-        TestExample();
-        
-        var options = WindowOptions.Default with
-        {
-            Size = new Vector2D<int>(800, 600),
-            Title = "My first Silk.NET application!"
-        };
-        
-        _window = Window.Create(options);
-        
-        _window.Load += OnLoad;
-        _window.Update += OnUpdate;
-        _window.Render += OnRender;
-        _window.Run();
-        
-        
+    private static readonly uint[] Indices = {
+        0, 1, 3,
+        1, 2, 3
+    };
 
+    private static void Main(string[] args) {
+        var options = WindowOptions.Default;
+        options.Size = new Vector2D<int>(800, 600);
+        options.Title = "LearnOpenGL with Silk.NET";
+        window = Window.Create(options);
+
+        window.Load += OnLoad;
+        window.Update += OnUpdate;
+        window.Render += OnRender;
+        window.Closing += OnClose;
+
+        window.Run();
+
+        window.Dispose();
     }
 
-
-    private static unsafe void OnLoad() {
-        IInputContext input = _window.CreateInput();
+    private static void OnLoad() {
+        IInputContext input = window.CreateInput();
         primaryKeyboard = input.Keyboards.FirstOrDefault();
-        if (primaryKeyboard != null)
-        {
+        if (primaryKeyboard != null) {
             primaryKeyboard.KeyDown += KeyDown;
         }
-        for (int i = 0; i < input.Mice.Count; i++)
-        {
+
+        for (int i = 0; i < input.Mice.Count; i++) {
             input.Mice[i].Cursor.CursorMode = CursorMode.Raw;
             input.Mice[i].MouseMove += OnMouseMove;
             input.Mice[i].Scroll += OnMouseWheel;
         }
-        
-        _gl = GL.GetApi(_window);
-        
 
-        _gl = _window.CreateOpenGL();
-        _gl.ClearColor(Color.CornflowerBlue);
-        
-        _vao = _gl.GenVertexArray();
-        _gl.BindVertexArray(_vao);
-        
-        _vbo = _gl.GenBuffer();
-        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
+        Gl = GL.GetApi(window);
 
-        fixed (float* buf = vertices)
-            _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(vertices.Length * sizeof(float)), buf,
-                BufferUsageARB.StaticDraw);
+        Ebo = new BufferObject<uint>(Gl, Indices, BufferTargetARB.ElementArrayBuffer);
+        Vbo = new BufferObject<float>(Gl, Vertices, BufferTargetARB.ArrayBuffer);
+        Vao = new VertexArrayObject<float, uint>(Gl, Vbo, Ebo);
 
-        _ebo = _gl.GenBuffer();
-        _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _ebo);
+        Vao.VertexAttributePointer(0, 3, VertexAttribPointerType.Float, 5, 0);
+        Vao.VertexAttributePointer(1, 2, VertexAttribPointerType.Float, 5, 3);
 
-        fixed (uint* buf = indices)
-            _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint)(indices.Length * sizeof(uint)), buf,
-                BufferUsageARB.StaticDraw);
+        Shader = new Shader(Gl, "shader.vert", "shader.frag");
 
-        const string vertexCode = @"
-#version 330 core
-
-layout (location = 0) in vec3 aPosition;
-
-void main()
-{
-    gl_Position = vec4(aPosition, 1.0);
-}";
-        
-        
-        const string fragmentCode = @"
-#version 330 core
-
-out vec4 out_color;
-
-void main()
-{
-    out_color = vec4(1.0, 0.5, 0.2, 1.0);
-}";
-        
-      
-        uint vertexShader = _gl.CreateShader(ShaderType.VertexShader);
-        _gl.ShaderSource(vertexShader, vertexCode);
-        
-        
-        _gl.CompileShader(vertexShader);
-
-        _gl.GetShader(vertexShader, ShaderParameterName.CompileStatus, out int vStatus);
-        if (vStatus != (int) GLEnum.True)
-            throw new Exception("Vertex shader failed to compile: " + _gl.GetShaderInfoLog(vertexShader));
-        
-        
-        uint fragmentShader = _gl.CreateShader(ShaderType.FragmentShader);
-        _gl.ShaderSource(fragmentShader, fragmentCode);
-
-        _gl.CompileShader(fragmentShader);
-
-        _gl.GetShader(fragmentShader, ShaderParameterName.CompileStatus, out int fStatus);
-        if (fStatus != (int) GLEnum.True)
-            throw new Exception("Fragment shader failed to compile: " + _gl.GetShaderInfoLog(fragmentShader));
-        
-        _program = _gl.CreateProgram();
-        
-        _gl.AttachShader(_program, vertexShader);
-        _gl.AttachShader(_program, fragmentShader);
-
-        _gl.LinkProgram(_program);
-
-        _gl.GetProgram(_program, ProgramPropertyARB.LinkStatus, out int lStatus);
-        if (lStatus != (int) GLEnum.True)
-            throw new Exception("Program failed to link: " + _gl.GetProgramInfoLog(_program));
-        
-        _gl.DetachShader(_program, vertexShader);
-        _gl.DetachShader(_program, fragmentShader);
-        _gl.DeleteShader(vertexShader);
-        _gl.DeleteShader(fragmentShader);
-        
-        
-        const uint positionLoc = 0;
-        _gl.EnableVertexAttribArray(positionLoc);
-        _gl.VertexAttribPointer(positionLoc, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), (void*) 0);
-        
-        _gl.BindVertexArray(0);
-        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
-        _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, 0);
- 
+        Texture = new Texture(Gl, "goldenTexture.jpg");
     }
 
+    private static unsafe void OnUpdate(double deltaTime) {
+        var moveSpeed = 2.5f * (float)deltaTime;
 
-
-    private static void OnUpdate(double dt) {
-        
-        var moveSpeed = 2.5f * (float) dt;
-
-        if (primaryKeyboard.IsKeyPressed(Key.W))
-        {
+        if (primaryKeyboard.IsKeyPressed(Key.W)) {
             //Move forwards
             CameraPosition += moveSpeed * CameraFront;
         }
-        if (primaryKeyboard.IsKeyPressed(Key.S))
-        {
+
+        if (primaryKeyboard.IsKeyPressed(Key.S)) {
             //Move backwards
             CameraPosition -= moveSpeed * CameraFront;
         }
-        if (primaryKeyboard.IsKeyPressed(Key.A))
-        {
+
+        if (primaryKeyboard.IsKeyPressed(Key.A)) {
             //Move left
             CameraPosition -= Vector3.Normalize(Vector3.Cross(CameraFront, CameraUp)) * moveSpeed;
         }
-        if (primaryKeyboard.IsKeyPressed(Key.D))
-        {
+
+        if (primaryKeyboard.IsKeyPressed(Key.D)) {
             //Move right
             CameraPosition += Vector3.Normalize(Vector3.Cross(CameraFront, CameraUp)) * moveSpeed;
         }
     }
 
-    private static unsafe void OnRender(double dt) {
-        _gl.Clear(ClearBufferMask.ColorBufferBit);
-        
-        _gl.BindVertexArray(_vao);
-        _gl.UseProgram(_program);
-        _gl.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, (void*) 0);
-        var projection = Matrix4x4.CreatePerspectiveFieldOfView(Calculate.DegreesToRadians(CameraZoom), (float) _window.Size.X / (float)_window.Size.Y, 0.1f, 100.0f);
-        
+    private static unsafe void OnRender(double deltaTime) {
+        Gl.Enable(EnableCap.DepthTest);
+        Gl.Clear((uint)(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit));
+
+        Vao.Bind();
+        Texture.Bind();
+        Shader.Use();
+        Shader.SetUniform("uTexture0", 0);
+
+        //Use elapsed time to convert to radians to allow our cube to rotate over time
+        var difference = (float)(window.Time * 100);
+
+        var model = Matrix4x4.CreateRotationY(Calculate.DegreesToRadians(difference)) * Matrix4x4.CreateRotationX(Calculate.DegreesToRadians(difference));
+        var view = Matrix4x4.CreateLookAt(CameraPosition, CameraPosition + CameraFront, CameraUp);
+        var projection = Matrix4x4.CreatePerspectiveFieldOfView(Calculate.DegreesToRadians(CameraZoom), Width / Height, 0.1f,
+                100.0f);
+
+        Shader.SetUniform("uModel", model);
+        Shader.SetUniform("uView", view);
+        Shader.SetUniform("uProjection", projection);
+
+        //We're drawing with just vertices and no indices, and it takes 36 vertices to have a six-sided textured cube
+        Gl.DrawArrays(PrimitiveType.Triangles, 0, 36);
     }
 
-
-
-    #region Input Control Methods
-
-    private static void KeyDown(IKeyboard keyboard, Key key, int keyCode) {
-        if (key == Key.Escape)
-            _window.Close();
-    }
-    
-    private static unsafe void OnMouseWheel(IMouse mouse, ScrollWheel scrollWheel)
-    {
-        
-        CameraZoom = Math.Clamp(CameraZoom - scrollWheel.Y, 1.0f, 45f);
-    }
-    
-    private static unsafe void OnMouseMove(IMouse mouse, Vector2 position)
-    {
+    private static unsafe void OnMouseMove(IMouse mouse, Vector2 position) {
         var lookSensitivity = 0.1f;
-        if (LastMousePosition == default)
-        {
+        if (LastMousePosition == default) {
             LastMousePosition = position;
         }
-        else
-        {
+        else {
             var xOffset = (position.X - LastMousePosition.X) * lookSensitivity;
             var yOffset = (position.Y - LastMousePosition.Y) * lookSensitivity;
             LastMousePosition = position;
@@ -244,7 +189,7 @@ void main()
             CameraYaw += xOffset;
             CameraPitch -= yOffset;
 
-            // lock rotation for natural camera behaviour
+            //We don't want to be able to look behind us by going over our head or under our feet so make sure it stays within these bounds
             CameraPitch = Math.Clamp(CameraPitch, -89.0f, 89.0f);
 
             CameraDirection.X = MathF.Cos(Calculate.DegreesToRadians(CameraYaw)) * MathF.Cos(Calculate.DegreesToRadians(CameraPitch));
@@ -254,19 +199,22 @@ void main()
         }
     }
 
-    #endregion
-
-    
-    
-    
-    
-
-
-    private static void TestExample() {
-        var obj = new Parser("MoebiusBand.obj");
-        Console.WriteLine("adsfjsdvfsdf");
+    private static unsafe void OnMouseWheel(IMouse mouse, ScrollWheel scrollWheel) {
+        //We don't want to be able to zoom in too close or too far away so clamp to these values
+        CameraZoom = Math.Clamp(CameraZoom - scrollWheel.Y, 1.0f, 45f);
     }
-    
-    
-    
+
+    private static void OnClose() {
+        Vbo.Dispose();
+        Ebo.Dispose();
+        Vao.Dispose();
+        Shader.Dispose();
+        Texture.Dispose();
+    }
+
+    private static void KeyDown(IKeyboard keyboard, Key key, int arg3) {
+        if (key == Key.Escape) {
+            window.Close();
+        }
+    }
 }
